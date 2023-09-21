15d19324c2a287b82a7412bc19a7025b328ab9e6
imballinst <imballinst@users.noreply.github.com>
2023-09-21 15:22:33 +0000

    deploy: ce019585c5a3f5e542f3025e7710495940c9c7bd

---
+++ astro/_astro/ResetPasswordForm.9be784d8.js
@@ -0,0 +1,1 @@
+import{j as s,F as e,I as t,B as o}from"./index.ebfe2af9.js";import"./index.e0b1b188.js";function m(){return s.jsxs(e,{onFinish:r=>{console.info(r)},children:[s.jsx(e.Item,{name:"email",label:"Email",rules:[{required:!0}],children:s.jsx(t,{})}),s.jsx(o,{htmlType:"submit",children:"Reset password"})]})}export{m as ResetPasswordForm};
